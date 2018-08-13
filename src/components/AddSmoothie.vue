2ad0009c76422c90b079070435d5a6d48f814199
Timothy Barland <tbarland77@hotmail.com>
2018-08-13 19:17:04 -0400

    smoothies can now be added using the firestore db

---
+++ src/components/AddSmoothie.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="add-smoothie container z-depth-1">
+    <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
+    <form @submit.prevent="addSmoothie">
+      <div class="field title">
+        <label for="title">Smoothie title:</label>
+        <input type="text" name="title" v-model="title">
+      </div>
+      <div v-for="(ing, index) in ingredients" class="field ingredient" :key="index">
+        <label for="ingredient">Ingredient:</label>
+        <input type="text" name="ingredient" v-model="ingredients[index]">
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an ingredient (press tab to add):</label>
+        <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
+      </div>
+      <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
+        <button class="btn pink">Add Smoothie</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import db from '@/firebase/init'
+import slugify from 'slugify'
+export default {
+  name: 'AddSmoothie',
+  data(){
+    return{
+      title: null,
+      ingredients: [],
+      another: null,
+      feedback: null,
+      slug: null
+    }
+  },
+  methods: {
+    addSmoothie(){
+        // does not add last smoothie due to tabbing
+      if(this.title){
+        this.feedback = null
+        // create a slug
+        this.slug = slugify(this.title, {
+          replacement: '-',
+          remove: /[$*_+~.()'"!\-:@]/g,
+          lower: true
+        })
+        console.log(this.slug)
+        //save smoothie to firestore
+        db.collection('smoothies').add({
+          title: this.title,
+          ingredients: this.ingredients,
+          slug: this.slug
+        }).then(() => {
+          this.$router.push({ name: 'Index' })
+        }).catch(err => {
+          console.log(err)
+        })
+      } else {
+        this.feedback = 'You must enter a smoothie title'
+      }
+    },
+    addIng(){
+      if(this.another){
+        this.ingredients.push(this.another)
+        this.another = null
+        this.feedback = null
+      } else {
+        this.feedback = 'You must enter a value to add another ingredient'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.add-smoothie{
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
+.add-smoothie h2{
+  font-size: 2em;
+  margin: 20px auto;
+}
+.add-smoothie .field{
+  margin: 20px auto;
+}
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
-
+import AddSmoothie from '@/components/AddSmoothie'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/add-smoothie',
+      name: 'AddSmoothie',
+      component: AddSmoothie
     }
   ]
 })
